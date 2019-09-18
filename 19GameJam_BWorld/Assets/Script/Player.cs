3221427fc22440f15e453c84a0b815fb380f40e8
HiroyaMasuko <c18015@std.it-college.ac.jp>
2019-09-18 11:01:00 +0900

    カメラがプレイヤーを追従するようにした。ついでにそれをパネルにも貼って背景が切れるのを阻止

--- 19GameJam_BWorld/Assets/Script/Player.cs
+++ 19GameJam_BWorld/Assets/Script/Player.cs
@@ -4,17 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private LayerMask m_WhatIsGround; // 追加
+    [SerializeField] private LayerMask m_WhatIsGround;
 
-    public float Jamp = 10f;
+
     public float Speed = 3f;
-    bool floating = true;
-
-    public bool isGround;
-
-    public float flameCount; //経過フレーム数
-
-
+    bool floating = true;//ジャンプの判定
+    public float flameCount; //ジャンプ力
 
     void FixedUpdate()
     {
@@ -30,35 +25,21 @@ public class Player : MonoBehaviour
 
         var foot_pos = new Vector2(transform.position.x, transform.position.y - 0.6f);
         //bool floating = (Physics2D.OverlapPoint(foot_pos, m_WhatIsGround) == null); 
-        if (floating && Input.GetKey(KeyCode.Space))
-        {
 
-            flameCount = flameCount + 50 * Time.deltaTime;
+        if (floating && Input.GetKey(KeyCode.Space))//ジャンプ判定がtrueでSpaceを押している間の処理
+        {
+            flameCount = flameCount + 50 * Time.deltaTime;//押している間flameCountが増える
             //Debug.Log(flameCount);
-
-            if(flameCount >= 11)
-            {
-                flameCount = 10;
-            }
+            if (flameCount >= 11) flameCount = 10;//ジャンプ力の上限
         }
 
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.Space))//Spaceを離した時の処理
         {
-
             var rb = GetComponent<Rigidbody2D>();
-            rb.AddForce(new Vector2(0f, flameCount), ForceMode2D.Impulse);
-            floating = false;
-
-            flameCount = 0; //フレームカウントをリセットする。
-            
+            rb.AddForce(new Vector2(0f, flameCount), ForceMode2D.Impulse);//addforceでFlameCount分、上に移動
+            floating = false;       //ジャンプ判定をoff
+            flameCount = 0;         //フレームカウントをリセットする。       
         }
-        
-
-        /*
-        var rb = GetComponent<Rigidbody2D>();
-        rb.AddForce(new Vector2(0f, Jamp), ForceMode2D.Impulse);
-        floating = false;*/
-
     }
 
     void OnCollisionEnter2D(Collision2D collision)
