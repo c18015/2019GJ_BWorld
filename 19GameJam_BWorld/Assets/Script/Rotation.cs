5fbb21ad9d4de3936c3d96ef0eb8598c818f4704
c19018 <c19018@std.it-college.ac.jp>
2019-09-18 12:25:07 +0900

    ブロック回転するスクリプト（Rotの値に"１"か"-１"で回転方向変わります）

---
+++ 19GameJam_BWorld/Assets/Script/Rotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    float speed;
+    public float rot;
+
+    void Update()
+    {
+        transform.Rotate(0, 0, rot * 50 * Time.deltaTime);
+    }
+}
